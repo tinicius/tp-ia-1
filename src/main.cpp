67967eab3adb3d4eb52973cf1c61f92894e0cfba
czarhrs <cesarresende.soares@hotmail.com>
2024-10-29 17:34:26 -0300

    feat: implemented dfs

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,72 @@
 #include <vector>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
-vector<pair<int, int>> adj[5][5];
+vector<pair<int, int> > adj[5][5];
 
 bool vis[5][5];
 
-int bfs(pair<int, int> start)
-{
-
-    queue<pair<int, int>> q;
+void bfs(pair<int, int> start) {
+    queue<pair<int, int> > q;
     q.push(start);
 
-    vis[start.first][start.second] = 1;
+    vis[start.first][start.second] = true;
 
-    while (!q.empty())
-    {
-
+    while (!q.empty()) {
         auto [x, y] = q.front();
         q.pop();
 
-        for (auto [u, v] : adj[x][y])
-        {
-
-            if (!vis[u][v])
-            {
-                vis[u][v] = 1;
+        for (auto [u, v]: adj[x][y]) {
+            if (!vis[u][v]) {
+                vis[u][v] = true;
                 q.push({u, v});
             }
         }
     }
 }
 
-int main()
-{
+void dfs(pair<int, int> start) {
+    stack<pair<int, int>> s;
+    s.push(start);
 
+    while (!s.empty()) {
+        auto [x, y] = s.top();
+        s.pop();
+
+        if (!vis[x][y]) {
+            vis[x][y] = true;
+
+            for (auto [u, v] : adj[x][y]) {
+                if (!vis[u][v]) {
+                    s.push({u, v});
+                }
+            }
+        }
+    }
+}
+
+int main() {
     adj[0][0] = {{0, 1}, {1, 0}}; // A
-    adj[0][1] = {{0, 0}};         // B
+    adj[0][1] = {{0, 0}}; // B
     adj[0][2] = {{0, 3}, {1, 2}}; // C
     adj[0][3] = {{0, 2}, {0, 4}}; // D
     adj[0][4] = {{0, 3}, {1, 4}}; // E
 
     // Segunda linha
-    adj[1][0] = {{0, 0}, {1, 1}};         // F
-    adj[1][1] = {{1, 0}, {1, 2}};         // G
+    adj[1][0] = {{0, 0}, {1, 1}}; // F
+    adj[1][1] = {{1, 0}, {1, 2}}; // G
     adj[1][2] = {{0, 2}, {1, 1}, {1, 3}}; // H
-    adj[1][3] = {{1, 2}, {2, 3}};         // I
-    adj[1][4] = {{0, 4}};                 // J
+    adj[1][3] = {{1, 2}, {2, 3}}; // I
+    adj[1][4] = {{0, 4}}; // J
 
     // Terceira linha
-    adj[2][0] = {{3, 0}};                 // K
-    adj[2][1] = {{2, 2}, {3, 1}};         // L
+    adj[2][0] = {{3, 0}}; // K
+    adj[2][1] = {{2, 2}, {3, 1}}; // L
     adj[2][2] = {{0, 4}, {2, 1}, {2, 3}}; // M
     adj[2][3] = {{2, 4}, {2, 2}, {1, 3}}; // N
-    adj[2][4] = {{2, 3}};                 // O
+    adj[2][4] = {{2, 3}}; // O
 
     // Quarta linha
     adj[3][0] = {{2, 0}, {4, 0}}; // P
@@ -64,14 +76,13 @@ int main()
     adj[3][4] = {{3, 3}, {4, 4}}; // T
 
     // Quinta linha
-    adj[4][0] = {{3, 0}, {4, 1}};         // U (Start)
+    adj[4][0] = {{3, 0}, {4, 1}}; // U (Start)
     adj[4][1] = {{4, 0}, {3, 1}, {4, 2}}; // V
-    adj[4][2] = {{4, 1}, {4, 3}};         // X
-    adj[4][3] = {{4, 2}, {4, 4}};         // Y
-    adj[4][4] = {{4, 3}, {3, 4}};         // Z
-
-
+    adj[4][2] = {{4, 1}, {4, 3}}; // X
+    adj[4][3] = {{4, 2}, {4, 4}}; // Y
+    adj[4][4] = {{4, 3}, {3, 4}}; // Z
 
     bfs({4, 0});
-}
+    dfs({4, 0});
 
+}
