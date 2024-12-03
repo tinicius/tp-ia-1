80587dc4164d280a41256d655f74a8620dd2cf3d
czarhrs <cesarresende.soares@hotmail.com>
2024-12-03 11:31:32 -0300

    feat: created .h files

---
+++ src/bfs.h
@@ -0,0 +1,8 @@
+//
+// Created by czarhrs on 03/12/2024.
+//
+
+#ifndef BFS_H
+#define BFS_H
+
+#endif //BFS_H
---
+++ src/dfs.h
@@ -0,0 +1,8 @@
+//
+// Created by czarhrs on 03/12/2024.
+//
+
+#ifndef DFS_H
+#define DFS_H
+
+#endif //DFS_H
