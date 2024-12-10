6ca29f4fe2dddac6e51af3c3d20fef8b566c0a7f
czarhrs <cesarresende.soares@hotmail.com>
2024-12-10 08:30:14 -0300

    bomba hehe

--- src/bfs.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by czarhrs on 03/12/2024.
-//
-
-#ifndef BFS_H
-#define BFS_H
-
-#endif //BFS_H
--- src/dfs.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by czarhrs on 03/12/2024.
-//
-
-#ifndef DFS_H
-#define DFS_H
-
-#endif //DFS_H
