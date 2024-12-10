e1baca920f198022481e2229c595e8a95097fa50
Vinicius Alves Pereira <vinicius.apereira04@gmail.com>
2024-12-10 18:10:57 -0300

    greedy

---
+++ build/CMakeFiles/tp-ia-1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vinicius/Documents/projects/tp-ia-1/src/astar.cpp" "CMakeFiles/tp-ia-1.dir/src/astar.cpp.o" "gcc" "CMakeFiles/tp-ia-1.dir/src/astar.cpp.o.d"
+  "/home/vinicius/Documents/projects/tp-ia-1/src/bfs.cpp" "CMakeFiles/tp-ia-1.dir/src/bfs.cpp.o" "gcc" "CMakeFiles/tp-ia-1.dir/src/bfs.cpp.o.d"
+  "/home/vinicius/Documents/projects/tp-ia-1/src/dfs.cpp" "CMakeFiles/tp-ia-1.dir/src/dfs.cpp.o" "gcc" "CMakeFiles/tp-ia-1.dir/src/dfs.cpp.o.d"
+  "/home/vinicius/Documents/projects/tp-ia-1/src/main.cpp" "CMakeFiles/tp-ia-1.dir/src/main.cpp.o" "gcc" "CMakeFiles/tp-ia-1.dir/src/main.cpp.o.d"
+  "/home/vinicius/Documents/projects/tp-ia-1/src/utils.cpp" "CMakeFiles/tp-ia-1.dir/src/utils.cpp.o" "gcc" "CMakeFiles/tp-ia-1.dir/src/utils.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tp-ia-1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tp-ia-1.
+# This may be replaced when dependencies are built.
